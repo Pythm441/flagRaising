36486d588fd316528fc43ad4804c32886c5054c7
Pythm441 <Abdulrahmanmetawea6@gmail.com>
2023-06-24 14:25:22 +0400

    Initial Commit

---
+++ cookieEater/cookieEaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookieEaterApp.swift
+//  cookieEater
+//
+//  Created by Abdelrahman Hesham on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct cookieEaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
